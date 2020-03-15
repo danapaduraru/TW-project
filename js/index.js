d711433aafe886131a71f93a801bb2e1d9b4cb5c
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2020-03-15 13:58:31 +0200

    added login and register forms

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
 function openNav() {
-    document.getElementById("sidenav").style.width = "225px";
-    document.getElementById("main-container").style.marginLeft = "225px";
-    // document.body.style.backgroundColor = "rgba(0,0,0, 0.3)";
+  document.getElementById("sidenav").style.width = "225px";
+  document.getElementById("main-container").style.marginLeft = "225px";
+  // document.body.style.backgroundColor = "rgba(0,0,0, 0.3)";
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+  document.getElementById("main-container").style.marginLeft = "0px";
+  document.body.style.backgroundColor = "#fff";
+}
+
+function triggerPopUp(popUpName, closeBtnName) {
+  var popUp = document.getElementById(popUpName);
+  var closeBtn = document.getElementsByClassName(closeBtnName)[0];
+
+  if (popUp.style.display == "" || popUp.style.display == "none") {
+    popUp.style.display = "block";
   }
-  
-  function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-    document.getElementById("main-container").style.marginLeft= "0px";
-    document.body.style.backgroundColor = "#fff";
+
+  closeBtn.onclick = function () {
+    popUp.style.display = "none";
   }
+
+  window.onclick = function (event) {
+    if (event.target == popUp) {
+      popUp.style.display = "none";
+    }
+  }
+}
