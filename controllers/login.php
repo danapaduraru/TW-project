f51831813a6f61ff0fd3badce6a8f4a1b13e00d3
Georgescu Florin <georgescuflorin24@gmail.com>
2020-04-29 17:55:43 +0300

    prevented sql injection

--- controllers/login.php
+++ controllers/login.php
@@ -2,16 +2,16 @@
 require_once('../models/User.php');
 $connection = Connection::Instance();
 
-if (isset($_POST['r_submit'])) {
-    $fullname = mysqli_real_escape_string($connection, $_POST['r_fullname']);
-    $email = mysqli_real_escape_string($connection, $_POST['r_email']);
-    $password = mysqli_real_escape_string($connection, $_POST['r_password']);
-    
+if (isset($_POST['l_submit'])) {
+
+    $email = mysqli_real_escape_string($connection, $_POST['l_email']);
+    $password = mysqli_real_escape_string($connection, $_POST['l_password']);
+
     // Create User object
     $user = new User($fullname, $email, $password);
     
     // Try to register user
-    if($user->registerUser()) {
+    if($user->loginUser()) {
         // If query was successful, redirect to dashboard
         header('Location: ../views/dashboard.html');
     }
