b987c6c91b7f42d670e9b35c0c526ebd8ca63920
Georgescu Florin <georgescuflorin24@gmail.com>
2020-06-10 22:41:50 +0300

    update rss

--- views/rsstop.php
+++ views/rsstop.php
@@ -1,5 +1,4 @@
 <?php
-    header("Content-Type: application/rss+xml; charset=ISO-8859-1");
 
     require_once('../models/Connection.php');
 
@@ -12,18 +11,18 @@
     $query1 = "SELECT p.name, COUNT(a.id_plant) FROM plant p JOIN plant_album a on p.id=a.id_plant GROUP BY a.id_plant ORDER BY COUNT(a.id_plant) DESC LIMIT 10;";
     $result1 = mysqli_query($connection, $query1);
 
-    $base_url = "http://localhost/TW-project/views/rsstop.php";
+    header("Content-Type: application/rss+xml; charset=ISO-8859-1");
+
+    $base_url = "http://localhost/TW-project/views/";
 
     echo "<?xml version='1.0' encoding='UTF-8' ?>" . PHP_EOL;
     echo "<rss version='2.0'>".PHP_EOL;
     echo "<channel>".PHP_EOL;
 
-
     echo "<title>Top Plants feed | RSS</title>".PHP_EOL;
-    echo "<link>".$base_url."index.php</link>".PHP_EOL;
+    echo "<link>".$base_url."rsstop.php</link>".PHP_EOL;
     echo "<description>Top plants, albums and users from PlanOr</description>".PHP_EOL;
     echo "<language>en-us</language>".PHP_EOL;
-    echo "This is a &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; lot of space.";
 
         echo "<title>Most popular plants</title>".PHP_EOL;
 
